7d2d484d1b0d4258be03c26b9e2e6af1241e5aa5
dionisio300 <dionisioassis80@gmail.com>
2025-09-24 18:31:57 -0300

    Aula 06

--- Flask/Aula13/app.py
+++ Flask/Aula13/app.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 def paginaInicial():
     return render_template('index.html')
 
-
+@app.route('/atividade')
+def atividade():
+    return render_template('atividade.html')
 
 app.run(debug=True)
